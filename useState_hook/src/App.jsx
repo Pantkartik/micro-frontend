10aee39fab6a69598c760913af31e4da3758d062
Kartik Pant <kartikpant.kp69@gmail.com>
2026-08-10 01:07:01 +0530

    feat: scaffold new destructure_js micro-frontend project with Vite and React

--- useState_hook/src/App.jsx
+++ useState_hook/src/App.jsx
@@ -1,26 +1,67 @@
-import { useState } from "react";
-import React from "react";
+// counter 
+import React, { useState } from "react";
 
 const App=()=>{
-  // setting up of useState for changing the value of the number
-  const[a,setA]=useState(10);
-  const[username,setUsername]=useState('Kartik')
-  function ChangeA(){
-    setA(a+5);
+  const[num,setNum]=useState(1);
+
+  function IncreaseNum(){
+    setNum(num + 1);
   }
-  function ChangeUsername(){
-    setUsername('naman')
+  
+  function DecreaseNum(){
+    setNum(num - 1);
   }
-  return(
+
+  return (
     <>
-    <h1>The value of a is {a}</h1>
-    <button onClick={ChangeA}> Click on me </button>
-    
-    <h1>The name is {username}</h1>
-    <button onClick={ChangeUsername}> change username </button>
+    <h1> The number is {num}</h1>
+    <br />
+    <button onClick={IncreaseNum}>Increase</button>
+    <br />
+    <button onClick={DecreaseNum}>Decrease</button>
+
+
     </>
   )
 }
 
 
+
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+// import { useState } from "react";
+// import React from "react";
+
+// const App=()=>{
+//   // setting up of useState for changing the value of the number
+//   const[a,setA]=useState(10);
+//   const[username,setUsername]=useState('Kartik')
+//   function ChangeA(){
+//     setA(a+5);
+//   }
+//   function ChangeUsername(){
+//     setUsername('naman')
+//   }
+//   return(
+//     <>
+//     <h1>The value of a is {a}</h1>
+//     <button onClick={ChangeA}> Click on me </button>
+    
+//     <h1>The name is {username}</h1>
+//     <button onClick={ChangeUsername}> change username </button>
+//     </>
+//   )
+// }
+
+
+// export default App;
